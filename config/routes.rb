c06d6301d24ae38e6e9d7cd3289fee1739eb941e
Mujavid Bukhari <sirmuj@gmail.com>
2016-10-31 15:20:51 +0000

    Added models (User, Post) and controller (Post)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/create'
+
+  get 'posts/update'
+
+  get 'posts/delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
