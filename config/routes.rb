4cd1c012f2ec1fa17e0146b4b49ac6faed1021d7
Mujavid Bukhari <sirmuj@gmail.com>
2016-11-03 18:16:35 +0000

    Update logout url path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+
   get 'citations/new'
+
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users
   resources :sessions
   resources :citations
 
-  root 'sessions#new'
   resources :welcome
 end
