e9ecef5b7c52ea4043b7f45109610f58c81f376b
Mujavid Bukhari <sirmuj@gmail.com>
2016-11-04 03:17:28 +0000

    Add application js back into rails to fix delete citation bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   root 'sessions#new'
-
-  get 'citations/new'
-
   get 'logout', to: 'sessions#destroy', as: 'logout'
-
   resources :users
   resources :sessions
   resources :citations
 
-  resources :welcome
 end
