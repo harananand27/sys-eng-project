c64af387e9ecd45eaa42b005df60e7f422442f3b
nadiamahg <yaelmahgerefteh@gmail.com>
2016-11-04 12:08:59 +0000

    Added comments to clarify logic in models

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -1,4 +1,6 @@
 class Citation < ActiveRecord::Base
 	belongs_to :user
+
+# Must include title & link, notes optional      
 	validates_presence_of :title, :link
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
 	has_many :citations
 
 	has_secure_password
+
+# Cannot sign up with same email
 	validates_uniqueness_of :email
+
+# Email must be in correct format
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
